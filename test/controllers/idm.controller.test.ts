8420565b826118e18f64e81da71c457d71cf1dfa
Shashwat Tripathi <stripathix@gmail.com>
2017-08-24 17:22:57 +0530

    Test cases for idmcontroller and httphandle

---
+++ test/controllers/idm.controller.test.ts
@@ -0,0 +1,66 @@
+import * as chai from 'chai';
+import * as mocha from 'mocha';
+import { Request, Response } from 'express';
+import chaiHttp = require('chai-http');
+import app from '../../server/app';
+import APIRequest from '../../server/util/api-request';
+import IdmController from '../../server/controllers/idm.controller';
+
+const expect = chai.expect;
+const validTestUser = {
+    username: "simplifiedtest",
+    password: "614",
+};
+const invalidTestUser = {
+    username: "simplifiedtest",
+    password: "6142",
+};
+let apiRequest = new APIRequest();
+let idmController = new IdmController(apiRequest);
+chai.use(chaiHttp);
+
+describe('IDM controller test cases', () => {
+    var request:Request;
+    var response:Response;
+    it('idmController.getIdmData function is defined', () => {
+        expect(idmController.getIdmData).to.exist;
+        expect((typeof idmController.getIdmData)).to.equals('function');
+    });
+    it('idmController.getIdmAccessToken function is defined', () => {
+        expect(idmController.getIdmAccessToken).to.exist;
+        expect((typeof idmController.getIdmAccessToken)).to.equals('function');
+    });
+    it('getIdmAccessToken returns a Promise', () => {
+        request = {
+            body: validTestUser
+        };
+        var fetchDataReturnObj = idmController.getIdmAccessToken(request, response);
+            expect((typeof fetchDataReturnObj.then)).to.equals('function');
+        });
+    it('Valid user should return 200', () => {
+        request = {
+            body: validTestUser
+        };
+        return idmController.getIdmAccessToken(request, response).then(function(data){
+            expect(data.res.statusCode).to.equals(200);
+        });
+    });
+  it('InValid user should return 400', () => {
+    var request:Request;
+    var response:Response;
+    request = {
+        body: invalidTestUser
+    };
+    return idmController.getIdmAccessToken(request, response).then(function(data){
+        expect(data.res.statusCode).to.equals(400);
+    });
+  });
+  it('/api/idmLogin should return 400 for invalid user', () => {
+        return chai.request(app)
+            .post('/api/idmLogin')
+            .type('application/json')
+            .send(invalidTestUser).catch((err) => {
+                expect(err.response.statusCode).to.equals(400);
+              });;
+  });
+});
---
+++ test/util/http-handle.test.ts
@@ -0,0 +1,52 @@
+import * as chai from 'chai';
+import * as mocha from 'mocha';
+import chaiHttp = require('chai-http');
+import app from '../../server/app';
+import HttpHandle from '../../server/util/http-handle';
+import APIRequest from '../../server/util/api-request';
+import { ApplicationConfig as APP_CONFIG } from '../../server/config/app_config';
+const expect = chai.expect;
+let getParamName = require('../utility');
+let apiRequest = new APIRequest();
+let httpHandle = new HttpHandle(apiRequest);
+chai.use(chaiHttp);
+
+describe('HTTP handle test cases', () => {
+    it('fetchData function is defined', () => {
+        expect(httpHandle.fetchData).to.exist;
+        expect((typeof httpHandle.fetchData)).to.equals('function');
+    });
+    it('fetchData function accepts config and formData parameters', () => {
+        var paramsList = getParamName(httpHandle.fetchData);
+        expect(paramsList[0]).to.equals('config');
+        expect(paramsList[1]).to.equals('formData');
+    });
+    const jwtFormData = {
+        client_id: APP_CONFIG.CLIENT_ID,
+        client_secret: APP_CONFIG.CLIENT_SECRET,
+        grant_type: APP_CONFIG.GRANT_TYPE,
+        scope: APP_CONFIG.SCOPE
+    },
+    jwtConfig = {
+        uri: APP_CONFIG.IDM_JWT_URL,
+        rejectMsg: 'Unable to fetch jwt token'
+    };   
+    it('fetchData function is defined', () => {
+        expect((typeof httpHandle.fetchData)).to.equals('function');
+    });
+    it('fetchData returns a Promise', () => {
+    var fetchDataReturnObj = httpHandle.fetchData(jwtConfig, jwtFormData);
+        expect((typeof fetchDataReturnObj.then)).to.equals('function');
+    });
+    it('fetchData for jwt token returns 200 status', () => {
+        var fetchDataReturnObj = httpHandle.fetchData(jwtConfig, jwtFormData);
+        return fetchDataReturnObj.then(function(data) {
+            expect(data.res.statusCode).to.equals(200);
+        }).catch(function(m) { throw new Error('FetchData'); });
+    });
+});
+
+describe('Fetch data returns pormise', () => {
+    
+  });
+  
