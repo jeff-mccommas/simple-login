97f3f29d6ee4af9bb270080cb460ab7e32397198
Jeffrey A McCommas <jeffmccommas@gmail.com>
2017-08-25 10:57:39 -0400

    tests running

--- src/app/services/prek12/prek12.service.spec.ts
+++ src/app/services/prek12/prek12.service.spec.ts
@@ -3,5 +3,5 @@ import {Prek12Service}from './prek12.service';
 let service :Prek12Service;
 describe('', function () {
 
-});it('should be a promise', () => {
+});it('should be a promise?', () => {
   });
