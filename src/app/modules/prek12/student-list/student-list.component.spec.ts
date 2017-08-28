a825480a59dd56e3c652b88d5a4ab12707c9c70a
jeff-mccommas <jeff.mccommas@mheducation.com>
2017-08-28 08:00:10 -0400

    studentListComponent
    studentListComponent

--- src/app/modules/prek12/student-list/student-list.component.spec.ts
+++ src/app/modules/prek12/student-list/student-list.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StudentListComponent } from './student-list.component';
 
 describe('StudentListComponent', () => {
-  let component: StudentListComponent;
-  let fixture: ComponentFixture<StudentListComponent>;
+    let component: StudentListComponent;
+    let fixture: ComponentFixture<StudentListComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ StudentListComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StudentListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            declarations: [ StudentListComponent ]
+        });
+        fixture = TestBed.createComponent(StudentListComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
