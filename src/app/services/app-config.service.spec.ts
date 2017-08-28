6649339402fe04cdbdeec158a7892529f69e2c66
jeff-mccommas <jeff.mccommas@mheducation.com>
2017-08-28 13:07:00 -0400

    ppConfigService
    ppConfigService

--- src/app/services/app-config.service.spec.ts
+++ src/app/services/app-config.service.spec.ts
@@ -1,13 +1,16 @@
-import { TestBed, inject } from '@angular/core/testing';
-
+import { inject } from '@angular/core/testing';
+import { Headers, Http } from '@angular/http';
 import { AppConfigService } from './app-config.service';
+let http: Http;
+let configService = new AppConfigService(http);
 
-describe('AppConfigService', () => {
+describe('AppConfigService Tests', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [AppConfigService]
-    });
   });
+    it(' should return a promise', function () {
+
+
+    });
 
   it('should ...', inject([AppConfigService], (service: AppConfigService) => {
     expect(service).toBeTruthy();
