d5ebe8478b9d6afadaaa5c7eb95a814133fc6941
jeff-mccommas <31095174+jeff-mccommas@users.noreply.github.com>
2017-08-28 07:33:46 -0400

    update prek12.service.spec.ts

--- src/app/services/prek12/prek12.service.spec.ts
+++ src/app/services/prek12/prek12.service.spec.ts
@@ -1,7 +1,21 @@
-import {Prek12Service}from './prek12.service';
+import {Prek12Service} from './prek12.service';
+import { Headers, Http } from '@angular/http';
 
-let service :Prek12Service;
-describe('', function () {
+let http: Http;
+let prek12Service = new Prek12Service(http);
+describe('Testing prek12.service', () => {
+    it('prek12Service.getSection function is defined', () => {
+        expect(prek12Service.getSection).toBeDefined();
+        expect((typeof prek12Service.getSection)).toBe('function');
+    });
 
-});it('should be a promise?', () => {
-  });
+    it('prek12Service.getSection function is returning a promise', () => {
+        let returnObj = prek12Service.getSection(1);
+        expect((typeof returnObj.then)).toBe('function');
+    });
+    it('prek12Service.getSection function is resolving valid response', () => {
+        return prek12Service.getSection(1).then(function(data){
+            //Assertion for this data test case
+        });
+    });
+});
