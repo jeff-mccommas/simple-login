13c4bbd7a5f3d0ac1d91e4d96d90dbecead97b9d
jeff-mccommas <jeff.mccommas@mheducation.com>
2017-08-28 13:05:23 -0400

    StudentListComponent

--- src/app/modules/prek12/student-list/student-list.component.spec.ts
+++ src/app/modules/prek12/student-list/student-list.component.spec.ts
@@ -1,8 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {By} from '@angular/platform-browser';
 
 import { StudentListComponent } from './student-list.component';
-import {by} from "protractor";
 describe('StudentListComponent', () => {
     let component: StudentListComponent;
     let fixture: ComponentFixture<StudentListComponent>;
